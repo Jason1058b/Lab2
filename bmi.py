9031bd562962d9dcc4dd5e91ba33c3ce808b35c0
Jason Tan <Jason.25@ichat.sp.edu.sg>
2026-05-07 11:00:23 +0800

    w

--- bmi.py
+++ bmi.py
@@ -1,5 +1,21 @@
 def calculate_bmi(weight, height):
-    print("Height = " + height)
-    print("Weight = " + weight)
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
 
-calculate_bmi(weight="57", height="1.73")
+    weight = float(weight)
+    height = float(height)
+
+    bmi = weight / (height ** 2)
+
+    if bmi < 18.5:
+        print("Under Weight, -1")
+    elif bmi <= 25.0:
+        print("Normal Weight, 0")
+    else:
+        print("Over Weight, 1")
+
+    return bmi
+
+
+bmi = calculate_bmi(weight="57", height="1.73")
+print("BMI = " + str(bmi))
